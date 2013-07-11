cddcbe3a414019bd42285cda4caa77f034f307fb
Ubuntu <ubuntu@ip-172-31-43-230.us-west-2.compute.internal>
2013-07-11 01:32:28 +0000

    touched a file

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var buf = fs.readFileSync("index.html");
   response.send(buf.toString('utf-8'));
-  //response.send('Hello World2!');
+  //response.send('Hello World22!');
 });
 
 var port = process.env.PORT || 5000;
